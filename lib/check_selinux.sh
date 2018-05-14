8bb5e0e29e568a38e7b5b183a75f80e8342bc039
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2018-05-14 19:23:41 +0900

    tentative commit (to test shUnit2)

--- lib/check_selinux.sh
+++ lib/check_selinux.sh
@@ -1,4 +1,10 @@
 function check_selinux() {
+    which getenforce 2>&/dev/null
+    if [ $? -eq 2 ]; then
+        state "System: 'getenforce' not found, skipping SELinux check. Run 'sudo yum install libselinux-utils' to fix." 2
+        return
+    fi
+
     # http://www.cloudera.com/content/www/en-us/documentation/enterprise/latest/topics/install_cdh_disable_selinux.html
     local msg="System: SELinux should be disabled"
     case $(getenforce) in
