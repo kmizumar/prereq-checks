ded5bca57fb2204bd3748923be0ba62a42aae773
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2018-05-14 20:04:43 +0900

    fixed the expected message
     WARN  System: 'getenforce' not found, skipping SELinux check. Run 'sudo yum install libselinux-utils' to fix.

--- test/check_selinux.sh
+++ test/check_selinux.sh
@@ -42,7 +42,7 @@ function test_check_selinux() {
     if is_centos_rhel_7; then
         result=$(rmescseq "$(check_selinux)")
         assertEquals "getenforce isn't included in CentOS7 image" \
-                     " FAIL  System: 'getenforce' not found, skipping SELinux check. Run 'sudo yum install libselinux-utils' to fix." \
+                     " WARN  System: 'getenforce' not found, skipping SELinux check. Run 'sudo yum install libselinux-utils' to fix." \
                      "${result}"
         sudo yum install libselinux-utils -y
     fi
